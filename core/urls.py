bf203a05ee5864214dbf019c004a9dea66a4c747
kleitomberg <ozkberguinho@gmail.com>
2021-12-15 17:56:00 -0300

    produto page

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, ProdutoView
 
 urlpatterns = [
-    path('',IndexView.as_view(), name='index')
+    path('',IndexView.as_view(), name='index'),
+    path('produto/',ProdutoView.as_view(), name='produto'),
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,6 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name='index.html'
 
+class ProdutoView(TemplateView):
+    template_name='produto.html'
+
