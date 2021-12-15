10b0d1f32451e7e407974f0e2822b082ec25f50d
kleitomberg <ozkberguinho@gmail.com>
2021-12-14 23:28:46 -0300

    carousel

---
+++ core/static/js/scripts.js
@@ -0,0 +1,84 @@
+
+var myCarousel = document.querySelector('#carouselExampleIndicators')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 750,
+    wrap: false,
+
+   
+
+})
+
+
+/*
+
+  setInterval(function(){ 
+  
+    var preto = '#212429'
+    var vermelho = '#CE2A50'
+    var verde = '#7ed957'
+    var amarelho = '#efd77a'
+ 
+    document.getElementById("navbar").style.backgroundColor = vermelho;
+  
+ 
+        
+ 
+ }, 2000);
+  
+
+
+
+
+setInterval(function(){ 
+    
+    var vermelho = '#CE2A50'
+    var verde = '#7ed957'
+    var amarelho = '#efd77a'
+
+    document.getElementById("navbar").style.backgroundColor = vermelho;
+
+       
+    
+
+}, 3000);
+
+setInterval(function(){ 
+        
+    var vermelho = '#f14953'
+    var roxo = '#6B15AC'
+    var amarelho = '#efd77a'
+
+    document.getElementById("navbar").style.backgroundColor = roxo;
+
+
+}, 4000);
+
+setInterval(function(){ 
+        
+    var vermelho = '#f14953'
+    var verde = '#A1C74B'
+    var amarelho = '#efd77a'
+
+    document.getElementById("navbar").style.backgroundColor = verde;
+
+
+}, 5000);
+
+setInterval(function(){ 
+        
+  var vermelho = '#f14953'
+  var azul = '#3C99D2'
+  var amarelho = '#efd77a'
+
+  document.getElementById("navbar").style.backgroundColor = azul;
+
+
+}, 6000);
+
+
+
+
+
+ 
+
+*/
